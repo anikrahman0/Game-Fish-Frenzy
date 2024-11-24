f9d096f881d1b1fc59d13db80cb267c50822bc22
anikrahman0 <a7604366@gmail.com>
2024-11-25 03:03:39 +0600

    fire & explosion animation effect, fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Select the canvas element
 const canvas = document.getElementById("myCanvas");
-canvas.width = 900;
+canvas.width = 1000;
 canvas.height = 500;
 // Get the 2D drawing context
 const ctx = canvas.getContext("2d");
@@ -77,24 +77,28 @@ class Particle{
         this.angle = 0
         this.va = Math.random() * 0.2 - 0.1
         this.bounced = 0
-        this.bottomBounceBoundary = Math.random() * 100 + 60
+        this.bottomBounceBoundary = Math.random() * 80 + 60
     }
 
     update(){
         this.angle += this.va
         this.speedY += this.gravity
-        this.x -= this.speedX
+        this.x -= this.speedX + this.game.speed
         this.y += this.speedY
         if(this.y > this.game.height + this.size || this.x < 0 - this.size){
             this.markForDeletion = true
         }
-        if (this.y > this.game.height - this.bottomBounceBoundary && this.bounced < 2){
+        if (this.y > this.game.height - this.bottomBounceBoundary && this.bounced < 5){
             this.bounced ++
             this.speedY *= -0.7
         }
     }
     draw(context){
-        context.drawImage(this.image, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize, this.spriteSize ,this.x, this.y, this.size, this.size)
+        context.save()
+        context.translate(this.x, this.y)
+        context.rotate(this.angle)
+        context.drawImage(this.image, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize, this.spriteSize, this.size * -0.5, this.size * -0.5, this.size, this.size)
+        context.restore()
     }
 }
 
@@ -178,7 +182,7 @@ class Player {
     enterPowerUp(){
         this.powerUpTimer  = 0
         this.powerUp = true
-        this.game.ammo = this.game.ammoMax
+        if (this.game.ammo < this.game.ammoMax) this.game.ammo = this.game.ammoMax
     }
 }
 
@@ -222,7 +226,7 @@ class Angler1 extends Enemy{
         this.y =  Math.random() * (this.game.height  * 0.9 - this.height)
         this.image = document.getElementById('angler1')
         this.frameY = Math.floor(Math.random() * 3)
-        this.lives = 2
+        this.lives = 5
         this.score = this.lives
     }
 }
@@ -232,10 +236,10 @@ class Angler2 extends Enemy {
         super(game)
         this.width = 213
         this.height = 165
-        this.y = Math.random() * (this.game.height * 0.9 - this.height)
+        this.y = Math.random() * (this.game.height * 0.95 - this.height)
         this.image = document.getElementById('angler2')
         this.frameY = Math.floor(Math.random() * 2)
-        this.lives = 3
+        this.lives = 6
         this.score = this.lives
     }
 }
@@ -245,11 +249,43 @@ class LuckyFish extends Enemy {
         super(game)
         this.width = 99
         this.height = 95
-        this.y = Math.random() * (this.game.height * 0.9 - this.height)
+        this.y = Math.random() * (this.game.height * 0.95 - this.height)
         this.image = document.getElementById('lucky')
         this.frameY = Math.floor(Math.random() * 2)
-        this.lives = 3
+        this.lives = 5
         this.score = 15
+        this.type = 'lucky'
+    }
+}
+
+class Hivewhale extends Enemy {
+    constructor(game) {
+        super(game)
+        this.width = 400
+        this.height = 227
+        this.y = Math.random() * (this.game.height * 0.95 - this.height)
+        this.image = document.getElementById('hivewhale')
+        this.frameY = 0
+        this.lives = 20
+        this.score = this.lives
+        this.type = 'hive'
+        this.speedX =  Math.random() * -1.2  - 0.2
+    }
+}
+
+class Drone extends Enemy {
+    constructor(game, x, y) {
+        super(game)
+        this.width = 115
+        this.height = 95
+        this.x = x 
+        this.y = y 
+        this.image = document.getElementById('drone')
+        this.frameY = Math.floor(Math.random() * 2)
+        this.lives = 3
+        this.score = this.lives
+        this.type = 'drone'
+        this.speedX = Math.random() * -4.2 - 0.5
     }
 }
 
@@ -294,6 +330,56 @@ class Background {
     }
 }
 
+class Explosion{
+    constructor(game, x, y) {
+        this.game = game
+        this.x = x
+        this.y = y
+        this.frameX = 0
+        this.maxFrame = 8
+        this.spriteWidth = 200
+        this.spriteHeight = 200
+        this.width = this.spriteWidth
+        this.height = this.spriteHeight
+        this.x = x - this.width * 0.5
+        this.y = y - this.height * 0.5
+        this.fps = 30
+        this.timer = 0
+        this.interval = 1000/this.fps
+        this.markForDeletion = false
+    }
+
+    update(deltaTime){
+        this.x -= this.game.speed
+        if (this.timer > this.interval){
+            this.frameX++
+            this.timer = 0
+        }else{
+            this.timer += deltaTime
+        }
+
+        if (this.frameX > this.maxFrame){
+            this.markForDeletion = true
+        }
+    }
+    draw(context){
+        context.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+    }
+}
+
+class SmokeExplosion extends Explosion{
+    constructor(game, x, y){
+        super(game, x, y)
+        this.image = document.getElementById('smokeExplosion')
+    }
+}
+
+class FireExplosion extends Explosion {
+    constructor(game, x, y) {
+        super(game, x, y)
+        this.image = document.getElementById('fireExplosion')
+    }
+}
 
 class UI {
     constructor(game){
@@ -323,8 +409,7 @@ class UI {
                 message1 = 'Game Over'
                 message2 = 'Try Again'
             }
-        
-            
+
             context.font = '70px '+ this.fontFamily
             context.fillText(message1, this.game.width * .5, this.game.height * .5 - 20)
 
@@ -350,17 +435,18 @@ class Game {
         this.ammo = 30
         this.ammoMax = 50
         this.ammoTimer = 0
-        this.ammoInterval = 500
+        this.ammoInterval = 350
         this.enemyTimer = 0
-        this.enemyInterval = 1000
+        this.enemyInterval = 2000
         this.keys = []
         this.enemies = []
         this.particles = []
+        this.explosions = []
         this.score= 0
-        this.winningScore= 50
+        this.winningScore= 80
         this.gameOver = false
         this.gameTime = 0
-        this.timeLimit = 1000 * 60 * 2
+        this.timeLimit = 30000 
         this.speed = 1
         this.debug = false
     }
@@ -377,17 +463,21 @@ class Game {
         }else{
             this.ammoTimer += deltaTime 
         }
+        this.explosions.forEach(explosion => explosion.update(deltaTime))
+        this.explosions = this.explosions.filter(explosion => !explosion.markForDeletion)
+
         this.particles.forEach(particle => particle.update())
         this.particles = this.particles.filter(particle => !particle.markForDeletion)
         this.enemies.forEach(enemy =>{
             enemy.update()
             if (this.collisionCheck(this.player, enemy)) {
                 enemy.markForDeletion = true
-                for (let i = 0; i < 10; i++) {
+                this.addExplosion(enemy)
+                for (let i = 0; i < enemy.score; i++) {
                     this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
                 }
-                if(enemy.type = 'lucky') this.player.enterPowerUp()
-                else this.score--
+                if(enemy.type === 'lucky') this.player.enterPowerUp()
+                else if(!this.gameOver) this.score--
             }
             this.player.projectiles.forEach(projectile => {
                 if (this.collisionCheck(projectile, enemy)) {
@@ -395,12 +485,18 @@ class Game {
                     projectile.markForDeletion = true
                     this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
                     if(enemy.lives <= 0){
-                        enemy.markForDeletion = true
-                        for (let i = 0; i < 10; i++) {
+                        for (let i = 0; i < enemy.score; i++) {
                             this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
                         }
+                        enemy.markForDeletion = true
+                        this.addExplosion(enemy)
+                        if(enemy.type === 'hive') {
+                            for (let i = 0; i < 5; i++) {
+                                this.enemies.push(new Drone(this, enemy.x + Math.random() * enemy.width, enemy.y + Math.random() * enemy.height * 0.5))
+                            }
+                        }
                         if(!this.gameOver) this.score += enemy.score
-                        if(this.score > this.winningScore) this.gameOver = true
+                        // if(this.score > this.winningScore) this.gameOver = true
                     }
                 }
             })   
@@ -417,10 +513,11 @@ class Game {
     }
     draw(context){
         this.background.draw(context)
-        this.player.draw(context)
         this.ui.draw(context)
+        this.player.draw(context)
         this.particles.forEach(particle => particle.draw(context))
         this.enemies.forEach(enemy => { enemy.draw(context) })
+        this.explosions.forEach(explosion => explosion.draw(context))
         this.background.layer4.draw(context)
     }
     addEnemy(){
@@ -429,11 +526,22 @@ class Game {
             this.enemies.push(new Angler1(this))
         } else if(randomize < 0.6){
             this.enemies.push(new Angler2(this))
-        }else{
+        } else if (randomize < 0.7) {
+            this.enemies.push(new Hivewhale(this))
+        } else{
             this.enemies.push(new LuckyFish(this))
         }
     }
 
+    addExplosion(enemy){
+        const randomize = Math.random()
+        if (randomize < 0.5) {
+            this.explosions.push(new SmokeExplosion(this, enemy.x + enemy.width * 0.5 , enemy.y + enemy.height * 0.5))
+        }else{
+            this.explosions.push(new FireExplosion(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
+
+        }
+    }
     collisionCheck(rect1, rect2){
         return(
             rect1.x < rect2.x +rect2.width && 
@@ -452,9 +560,9 @@ function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime
     lastTime = timeStamp
     ctx.clearRect(0,0,canvas.width,canvas.height)
-    game.update(deltaTime)
     game.draw(ctx)
+    game.update(deltaTime)
     requestAnimationFrame(animate)
 }
 
-requestAnimationFrame(animate)
+animate(0)
